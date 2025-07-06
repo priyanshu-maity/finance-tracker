1f7364cc0f0821202009d539ac4470456dd66b1b
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2025-07-06 12:15:59 +0530

    feat: automate customer id input and continue

--- scraper/bstat_scraper.py
+++ scraper/bstat_scraper.py
@@ -1,5 +1,11 @@
 import undetected_chromedriver as uc
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
 from dotenv import load_dotenv
 import os
 import yaml
@@ -19,6 +25,19 @@ class BStatScraper:
         customer_id, password = self.load_details()
         selectors = self.load_selectors()
 
+        sleep(5)
+        self.driver.switch_to.frame(0)
+
+        customer_id_input: WebElement = WebDriverWait(self.driver, timeout=10).until(
+            EC.presence_of_element_located((By.XPATH, selectors['customer_id']))
+        )
+        self.smart_type(customer_id_input, customer_id)
+        sleep(random.uniform(1.5, 3.0))
+
+        WebDriverWait(self.driver, timeout=10).until(
+            EC.element_to_be_clickable((By.XPATH, selectors['continue_button']))
+        ).click()
+
     def close(self):
         self.driver.quit()
 
@@ -33,7 +52,14 @@ class BStatScraper:
             return yaml.safe_load(file)
 
     @staticmethod
-    def smart_type(element, text: str, min_delay: float = 0.05, max_delay: float = 0.15):
+    def smart_type(element: WebElement, text: str, min_delay: float = 0.2, max_delay: float = 0.5):
         for char in text:
             element.send_keys(char)
             sleep(random.uniform(min_delay, max_delay))
+
+if __name__ == '__main__':
+    scraper = BStatScraper(headless=False)
+    try:
+        scraper.scrape('https://netbanking.hdfcbank.com/netbanking/')
+    finally:
+        scraper.close()
